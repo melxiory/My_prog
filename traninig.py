7d69d6516adb350fc685b41f5da4dbf0f55453d4
melxiory <sibagatullinnail@gmail.com>
2021-05-12 15:05:32 +0300

    Codewars, 3 задача!

--- traninig.py
+++ traninig.py
@@ -1,24 +1,6 @@
-def duplicate_count(text):
-    if text.isalnum():
-        text = text.lower()
-        sum = 0
-        k = 0
-        while True:
-            if text.count(text[sum]) >= 2:
-                otsec = text[sum]
-                for i in text:
-                    if otsec == i:
-                        text = text[0:text.index(i)] + text[text.index(i)+1:len(text)]
-                k += 1
-            else:
-                sum += 1
-            if sum == len(text):
-                return k
-    else:
-        return 0
+def find_smallest_int(arr):
+    return min(arr)
 
+arr = [1, 1-2**10, 2**10]
 
-
-slov=input('Введите слово - ')
-
-print(duplicate_count(slov))
+print(find_smallest_int(arr))

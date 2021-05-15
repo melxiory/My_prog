0ce97a8b16c04722beb41a2c16ea64e8893f69f7
melxiory <sibagatullinnail@gmail.com>
2021-05-15 21:43:10 +0300

    Codewars, 5 задача!

--- traninig.py
+++ traninig.py
@@ -1,12 +1,4 @@
-def nb_year(p0, percent, aug, p):
-    sum = 0
-    while True:
-        p0 = p0+p0*percent/100+aug
-        if p > p0:
-            sum += 1
-        else:
-            sum += 1
-            break
-    return sum
+def digitize(n):
+    return [int(i) for i in list(str(n))[::-1]  ]
 
-print(nb_year(1500,5,100,5000))
+print(digitize(23423))

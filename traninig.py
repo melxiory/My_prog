1025a366bcf3601179f2cc3a17aac2989f2fd71f
melxiory <sibagatullinnail@gmail.com>
2021-05-28 17:45:34 +0300

    Codewars, 7 задача!

--- traninig.py
+++ traninig.py
@@ -1,6 +1,9 @@
-def sum_array(a):
-    return sum(a) if ''.join(str(i) for i in a).isdigit() else 0
-print(sum_array([1.1, 2.2, 3.3]))
+def find_even_index(arr):
+    for i in range(len(arr)):
+        a = sum(arr[0:i])
+        b = sum(arr[i+1:len(arr)])
+        if a == b:
+            return i
+    return -1
 
-strok = ''.join(str(i if i == int else int(i)) for i in [1.1, 2.2, 3.3]).isdigit()
-print(strok)
+print(find_even_index([10,-80,10,10,15,35,20]))

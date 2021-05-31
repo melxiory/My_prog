c115ada50a9b0a9f529993e3555854aaffb4399c
melxiory <sibagatullinnail@gmail.com>
2021-05-31 17:41:33 +0300

    Codewars, 8 задача!

--- traninig.py
+++ traninig.py
@@ -1,9 +1,15 @@
-def find_even_index(arr):
-    for i in range(len(arr)):
-        a = sum(arr[0:i])
-        b = sum(arr[i+1:len(arr)])
-        if a == b:
-            return i
-    return -1
+def wave(people):
+    spisok = []
+    for i in range(len(people)):
+        if 0 < i < len(people)-1:
+            if people[i] != ' ':
+                spisok.append(people[:i] + people.upper()[i] + people[i+1:])
+        elif i == 0:
+            if people[i] != ' ':
+                spisok.append(people.upper()[0] + people[1:])
+        else:
+            if people[i] != ' ':
+                spisok.append(people[:i] + people.upper()[i])
+    return spisok
 
-print(find_even_index([10,-80,10,10,15,35,20]))
+print(wave("hello"))

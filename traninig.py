93f80f2a2f0d64617093762522a12ea8b5dd2c4d
melxiory <sibagatullinnail@gmail.com>
2021-06-04 22:17:20 +0300

    Codewars, 10 задача, как правильно!

--- traninig.py
+++ traninig.py
@@ -1,22 +1,8 @@
 def increment_string(strng):
-    if strng.isalpha():
-        return strng + "1"
-    if strng.isdigit():
-        chislo = int(strng) + 1
-        if len(str(strng)) > len(str(chislo)):
-            return '0'*(len(str(strng)) - len(str(chislo))) + str(int(strng)+1)
-        return str(int(strng)+1)
-    if strng == '':
-        return strng + "1"
-    for i in range(len(strng)-1, -1, -1):
-        if strng[i].isdigit() == False:
-            strok = strng[i+1:]
-            chislo = int(strok) + 1
-            if len(strok) > len(str(chislo)):
-                return strng[:i+1] + '0'*(len(strok)-len(str(chislo))) + str(chislo)
-            else:
-                return strng[:i+1] + str(chislo)
-
+    head = strng.rstrip('0123456789')
+    tail = strng[len(head):]
+    if tail == "": return strng + "1"
+    return head + str(int(tail) + 1).zfill(len(tail))
 
 
 

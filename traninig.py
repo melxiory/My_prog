3383e8e426b294d37aea34b5d778fa5b9bd923ed
melxiory <sibagatullinnail@gmail.com>
2021-06-10 12:40:33 +0300

    Codewars, 12 задача!

--- traninig.py
+++ traninig.py
@@ -1,9 +1,8 @@
-def comp(array1, array2):
-    try:
-        return sorted([i ** 2 for i in array1]) == sorted(array2)
-    except:
-        return False
+def kebabize(string):
+    return ''.join(['-'+i if j!=0 and i.isupper() else i for j, i in enumerate(string) if i.isalpha()]).lower()
 
-print(comp([-121, -144, 19, -161, 19, -144, 19, -11], [11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19]))
-print(comp([121, 144, 19, 161, 19, 144, 19, 11], [11*21, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19]))
-print(comp([121, 144, 19, 161, 19, 144, 19, 11], [11*11, 121*121, 144*144, 190*190, 161*161, 19*19, 144*144, 19*19]))
+
+print(kebabize('myCamelCasedString'))
+print(kebabize('myCamelHas3Humps'))
+print(kebabize('SOS'))
+print(kebabize('42'))

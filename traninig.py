31b4df25e6b055b453faeaaed38f2741c38ebcef
melxiory <sibagatullinnail@gmail.com>
2021-06-15 21:54:57 +0300

    Codewars, 14 задача  как првильно!

--- traninig.py
+++ traninig.py
@@ -1,14 +1,12 @@
 def likes(names):
-    if names == []:
-        return 'no one likes this'
-    if len(names) == 1:
-        return f'{names[0]} likes this'
-    if len(names) == 2:
-        return f'{names[0]} and {names[1]} likes this'
-    if len(names) == 3:
-        return f'{names[0]}, {names[1]} and {names[2]} likes this'
-    if len(names) > 3:
-        return f'{names[0]}, {names[1]} and {len(names)-2} others likes this'
+    n = len(names)
+    return {
+        0: 'no one likes this',
+        1: '{} likes this',
+        2: '{} and {} like this',
+        3: '{}, {} and {} like this',
+        4: '{}, {} and {others} others like this'
+    }[min(4, n)].format(*names[:3], others=n-2)
 
 
 print(likes([]))

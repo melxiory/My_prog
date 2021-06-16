f09aaa5fb72971435c7b6430258c91ee070dd9b7
melxiory <sibagatullinnail@gmail.com>
2021-06-16 11:52:07 +0300

    Codewars, 16 задача как правильно!

--- traninig.py
+++ traninig.py
@@ -1,18 +1,8 @@
 def Xbonacci(signature, n):
-    if len(signature)< n:
-        sum_1 = 0
-        spis = signature[:]
-        for i in range(len(signature), n):
-            for j in range(1, len(signature)+1):
-                sum_1 += spis[i-j]
-            spis.append(sum_1)
-            sum_1 = 0
-    else:
-        if n:
-            spis = signature[:n]
-        else:
-            spis = []
-    return spis
+    output, x = signature[:n], len(signature)
+    while len(output) < n:
+        output.append(sum(output[-x:]))
+    return output
 
 print(Xbonacci([0,1],10))
 print(Xbonacci([1,1],10))

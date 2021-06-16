e9c8868ade2e61447302c3a76125151ad085181c
melxiory <sibagatullinnail@gmail.com>
2021-06-16 11:51:46 +0300

    Codewars, 16 задача!

--- traninig.py
+++ traninig.py
@@ -1,16 +1,21 @@
-def likes(names):
-    n = len(names)
-    return {
-        0: 'no one likes this',
-        1: '{} likes this',
-        2: '{} and {} like this',
-        3: '{}, {} and {} like this',
-        4: '{}, {} and {others} others like this'
-    }[min(4, n)].format(*names[:3], others=n-2)
+def Xbonacci(signature, n):
+    if len(signature)< n:
+        sum_1 = 0
+        spis = signature[:]
+        for i in range(len(signature), n):
+            for j in range(1, len(signature)+1):
+                sum_1 += spis[i-j]
+            spis.append(sum_1)
+            sum_1 = 0
+    else:
+        if n:
+            spis = signature[:n]
+        else:
+            spis = []
+    return spis
 
-
-print(likes([]))
-print(likes(['Peter']))
-print(likes(['Jacob', 'Alex']))
-print(likes(['Max', 'John', 'Mark']))
-print(likes(['Alex', 'Jacob', 'Mark', 'Max']))
+print(Xbonacci([0,1],10))
+print(Xbonacci([1,1],10))
+print(Xbonacci([0,0,0,0,1],10))
+print(Xbonacci([1,0,0,0,0,0,1],10))
+print(Xbonacci([1,0,0,0,0,0,0,0,0,0],20))

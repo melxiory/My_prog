cc534197b6e8d9c0ffda3da4f11d89face96f693
melxiory <sibagatullinnail@gmail.com>
2021-07-03 23:25:22 +0300

    Codewars, 18 как правильно.

--- traninig.py
+++ traninig.py
@@ -1,27 +1,22 @@
 def valid(a):
-    dlin1 = len(a[0])
-    dlin2 = len(a[0][0])
-    kol_players = ''.join(a[0])
-    if len(a[0][0]) % 2 == 1:
-        return False
-    for i in a:
-        if len(i) != dlin1:
-            return False
-        for j in i:
-            if len(j) != dlin2:
-                return False
-            for l in j:
-                if l not in kol_players:
-                    return False
-    spis_nach = [j for i in a for j in i]
-    spis = []
-    if len(a[0][0]) == 4:
-        for i in spis_nach:
-            for j in range(3):
-                spis.append(i[j]+i[j+1])
-    for i in spis:
-        if spis.count(i) >= 1 and spis.count(i[1]+i[0]) >= 1 or spis.count(i) > 1:
-            return False
+    d = {}
+    day_length = len(a[0])
+    group_size = len(a[0][0])
+    golfers = {g for p in a[0] for g in p}
+
+    for day in a:
+        if len(day) != day_length: return False
+        for group in day:
+            if len(group) != group_size: return False
+            for player in group:
+                if player not in golfers: return False
+                if player not in d:
+                    d[player] = set(group)
+                else:
+                    if len(d[player] & set(group)) > 1:
+                        return False
+                    else:
+                        d[player].add(group)
     return True
 
 print(valid([['ABCD', 'EFGH', 'IJKL', 'MNOP', 'QRST'], ['AEIM', 'BJOQ', 'CHNT', 'DGLS', 'FKPR'], ['AGKO', 'BIPT', 'CJMS', 'DHFR', 'ELNQ'], ['AHLP', 'NKBS', 'CEOR', 'DFIQ', 'MJGT'], ['AFJN', 'BLMR', 'CGPQ', 'DEKT', 'HIOS']]))

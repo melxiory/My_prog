2356efc0ed5f48c7c5f682f9564530a15e181ff1
melxiory <sibagatullinnail@gmail.com>
2021-07-03 23:24:31 +0300

    Codewars, 18

--- traninig.py
+++ traninig.py
@@ -1,41 +1,44 @@
-def land_perimeter(arr):
-    def numofneighbour(mat, i, j):
-        count = 0
-        # UP
-        if (i > 0 and mat[i - 1][j]):
-            count += 1
-        # LEFT
-        if (j > 0 and mat[i][j - 1]):
-            count += 1
-        # DOWN
-        if (i < R - 1 and mat[i + 1][j]):
-            count += 1
-        # RIGHT
-        if (j < C - 1 and mat[i][j + 1]):
-            count += 1
-        return count
-    arr = [list(i) for i in arr]
-    R = len(arr)
-    C = len(arr[0])
+def valid(a):
+    dlin1 = len(a[0])
+    dlin2 = len(a[0][0])
+    kol_players = ''.join(a[0])
+    if len(a[0][0]) % 2 == 1:
+        return False
+    for i in a:
+        if len(i) != dlin1:
+            return False
+        for j in i:
+            if len(j) != dlin2:
+                return False
+            for l in j:
+                if l not in kol_players:
+                    return False
+    spis_nach = [j for i in a for j in i]
     spis = []
-    for i in range(len(arr)):
-        for j in range(len(arr[i])):
-            if arr[i][j] == "X":
-                arr[i][j] = 1
-            else:
-                arr[i][j] = 0
-    perimeter = 0
-    for i in range(0, R):
-        for j in range(0, C):
-            if arr[i][j]:
-                perimeter += (4 - numofneighbour(arr, i, j))
-    return f"Total land perimeter: {perimeter}"
+    if len(a[0][0]) == 4:
+        for i in spis_nach:
+            for j in range(3):
+                spis.append(i[j]+i[j+1])
+    for i in spis:
+        if spis.count(i) >= 1 and spis.count(i[1]+i[0]) >= 1 or spis.count(i) > 1:
+            return False
+    return True
 
+print(valid([['ABCD', 'EFGH', 'IJKL', 'MNOP', 'QRST'], ['AEIM', 'BJOQ', 'CHNT', 'DGLS', 'FKPR'], ['AGKO', 'BIPT', 'CJMS', 'DHFR', 'ELNQ'], ['AHLP', 'NKBS', 'CEOR', 'DFIQ', 'MJGT'], ['AFJN', 'BLMR', 'CGPQ', 'DEKT', 'HIOS']]))
+print(valid([ ["AB"] ]))
+print(valid([
+    ["AB", "CD"],
+    ["AD", "BC"],
+    ["BD", "AC"]]))
+print(valid([
+    ['ABCD', 'EFGH', 'IJKL', 'MNOP', 'QRST'],
+    ['AEIM', 'BJOQ', 'CHNT', 'DGLS', 'FKPR'],
+    ['AGKO', 'BIPT', 'CFMS', 'DHJR', 'ELNQ'],
+    ['AHLP', 'BKNS', 'CEOR', 'DFIQ', 'GJMT'],
+    ['AFJN', 'BLMR', 'CGPQ', 'DEKT', 'HIOS']]))
+print(valid([
+    ["AB", "CD", "EF", "GH"],
+    ["AC", "BD", "EG", "FH"],
+    ["AD", "CE"],
+    ["AE", "BG", "CH", "FD"]]))
 
-
-
-print(land_perimeter(["OXOOOX", "OXOXOO", "XXOOOX", "OXXXOO", "OOXOOX", "OXOOOO", "OOXOOX", "OOXOOO", "OXOOOO", "OXOOXX"]))
-print(land_perimeter(["OXOOO", "OOXXX", "OXXOO", "XOOOO", "XOOOO", "XXXOO", "XOXOO", "OOOXO", "OXOOX", "XOOOO", "OOOXO"]))
-print(land_perimeter(["XXXXXOOO", "OOXOOOOO", "OOOOOOXO", "XXXOOOXO", "OXOXXOOX"]))
-print(land_perimeter(["XOOOXOO", "OXOOOOO", "XOXOXOO", "OXOXXOO", "OOOOOXX", "OOOXOXX", "XXXXOXO"]))
-print(land_perimeter(["OOOOXO", "XOXOOX", "XXOXOX", "XOXOOO", "OOOOOO", "OOOXOO", "OOXXOO"]))

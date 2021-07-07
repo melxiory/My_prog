de53e3247dbb64ea054f6179ebdb8e8ce01fc9e8
melxiory <sibagatullinnail@gmail.com>
2021-07-07 20:41:49 +0300

    Codewars, 19

--- traninig.py
+++ traninig.py
@@ -1,39 +1,27 @@
-def valid(a):
-    d = {}
-    day_length = len(a[0])
-    group_size = len(a[0][0])
-    golfers = {g for p in a[0] for g in p}
+def encode(cards):
+    dict_mast = {values: key for key, values in enumerate([x + y for y in ['c', 'd', 'h', 's']
+                                                           for x in
+                                                           ['A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q',
+                                                            'K']])}
+    spis_encode = sorted([dict_mast[i] for i in cards])
+    return spis_encode
 
-    for day in a:
-        if len(day) != day_length: return False
-        for group in day:
-            if len(group) != group_size: return False
-            for player in group:
-                if player not in golfers: return False
-                if player not in d:
-                    d[player] = set(group)
-                else:
-                    if len(d[player] & set(group)) > 1:
-                        return False
-                    else:
-                        d[player].add(group)
-    return True
 
-print(valid([['ABCD', 'EFGH', 'IJKL', 'MNOP', 'QRST'], ['AEIM', 'BJOQ', 'CHNT', 'DGLS', 'FKPR'], ['AGKO', 'BIPT', 'CJMS', 'DHFR', 'ELNQ'], ['AHLP', 'NKBS', 'CEOR', 'DFIQ', 'MJGT'], ['AFJN', 'BLMR', 'CGPQ', 'DEKT', 'HIOS']]))
-print(valid([ ["AB"] ]))
-print(valid([
-    ["AB", "CD"],
-    ["AD", "BC"],
-    ["BD", "AC"]]))
-print(valid([
-    ['ABCD', 'EFGH', 'IJKL', 'MNOP', 'QRST'],
-    ['AEIM', 'BJOQ', 'CHNT', 'DGLS', 'FKPR'],
-    ['AGKO', 'BIPT', 'CFMS', 'DHJR', 'ELNQ'],
-    ['AHLP', 'BKNS', 'CEOR', 'DFIQ', 'GJMT'],
-    ['AFJN', 'BLMR', 'CGPQ', 'DEKT', 'HIOS']]))
-print(valid([
-    ["AB", "CD", "EF", "GH"],
-    ["AC", "BD", "EG", "FH"],
-    ["AD", "CE"],
-    ["AE", "BG", "CH", "FD"]]))
+def decode(cards):
+    dict_mast = {key: values for key, values in enumerate([x + y for y in ['c', 'd', 'h', 's']
+                                                           for x in
+                                                           ['A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q',
+                                                            'K']])}
+    spis_decode = [dict_mast[i] for i in sorted(cards)]
+    return spis_decode
 
+
+print(encode(["Td", "8c", "Ks"]))
+print(encode(["Qh", "5h", "Ad"]))
+print(encode(["8c", "Ks", "Td"]))
+print(encode(["Qh", "Ad", "5h"]))
+
+print(decode([7, 22, 51]))
+print(decode([13, 30, 37]))
+print(decode([7, 51, 22]))
+print(decode([13, 37, 30]))

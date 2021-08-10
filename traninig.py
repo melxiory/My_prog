f6de48e6aed147fe11e102b5ec4745a10af92725
melxiory <sibagatullinnail@gmail.com>
2021-08-10 19:20:26 +0300

    Codewars, 25!!!

--- traninig.py
+++ traninig.py
@@ -1,26 +1,21 @@
-def f(n):
-    end_kol_chet = 0
-    if n % 2 == 1:
-        n -= 1
-    a = list(range(n + 1))
-    a[1] = 0
-    for i in a:
-        if i > 1:
-            for j in range(i + i, len(a), i):
-                a[j] = 0
-        i += 1
-    lst = [x for x in a if x != 0]
-    lst.reverse()
-    lst = lst[:int(len(lst)*0.3)]
-    for i in lst:
-        if len([j for j in str(i) if int(j) % 2 == 0]) > end_kol_chet:
-            end_kol_chet = len([j for j in str(i) if int(j) % 2 == 0])
-            end_chis = i
-    return end_chis
+import itertools
 
 
-print(f(4683621))
-print(f(3563331))
-print(f(2714150))
-print(f(500))
-print(f(487))
+def slogan_maker(array):
+    if not array: return []
+    array_end = []
+    for i in array:
+        if i not in array_end:
+            array_end.append(i)
+    if len(array) == 1: return array
+    spis_on_exit = []
+    for i in itertools.permutations(array_end):
+        spis_on_exit.append(i)
+    return [' '.join(i) for i in spis_on_exit]
+
+
+print(slogan_maker(["super"]))
+print(slogan_maker(["super", "hot"]))
+print(slogan_maker(["super", "hot", "guacamole"]))
+print(slogan_maker(["super", "guacamole", "super", "super", "hot", "guacamole"]))
+print(slogan_maker(["testing", "testing", "testing"]))

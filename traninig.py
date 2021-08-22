24ecd951b6a3e4a6afa532a47f1ed29fe3cdd29a
melxiory <sibagatullinnail@gmail.com>
2021-08-22 17:05:36 +0300

    Codewars, 26(регулярка)!!!

--- traninig.py
+++ traninig.py
@@ -1,21 +1,16 @@
-import itertools
+import re
 
 
-def slogan_maker(array):
-    if not array: return []
-    array_end = []
-    for i in array:
-        if i not in array_end:
-            array_end.append(i)
-    if len(array) == 1: return array
-    spis_on_exit = []
-    for i in itertools.permutations(array_end):
-        spis_on_exit.append(i)
-    return [' '.join(i) for i in spis_on_exit]
+def regular_expressions(n):
+    valid_date = re.search(r'\[((02-((1\d|0[1-9])|2[0-8]))|((1[0-2]|0([1]|[3-9]))-(([1-2]\d|0[1-9])|3[0-1])))]', n)
+    return valid_date.group(0)
 
-
-print(slogan_maker(["super"]))
-print(slogan_maker(["super", "hot"]))
-print(slogan_maker(["super", "hot", "guacamole"]))
-print(slogan_maker(["super", "guacamole", "super", "super", "hot", "guacamole"]))
-print(slogan_maker(["testing", "testing", "testing"]))
+print(regular_expressions("[[[08-29]]]"))
+print(regular_expressions("[01-23]"))
+print(regular_expressions("[02-16]"))
+print(regular_expressions("[02-31]"))
+print(regular_expressions("[ 6-03]"))
+print(regular_expressions("ignored [08-11] ignored"))
+print(regular_expressions("[3] [12-04] [09-tenth]"))
+print(regular_expressions("[02-00]"))
+print(regular_expressions("[02-[08-11]04]"))

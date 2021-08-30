589b79736595531992774307e284db74a6ff044d
melxiory <sibagatullinnail@gmail.com>
2021-08-30 18:19:48 +0300

    Codewars, 27!!!

--- traninig.py
+++ traninig.py
@@ -1,16 +1,90 @@
-import re
+def zero(arg=None):
+    if not arg:
+        return 0
+    else:
+        return eval(f'0{arg}')
 
 
-def regular_expressions(n):
-    valid_date = re.search(r'\[((02-((1\d|0[1-9])|2[0-8]))|((1[0-2]|0([1]|[3-9]))-(([1-2]\d|0[1-9])|3[0-1])))]', n)
-    return valid_date.group(0)
+def one(arg=None):
+    if not arg:
+        return 1
+    else:
+        return eval(f'1{arg}')
 
-print(regular_expressions("[[[08-29]]]"))
-print(regular_expressions("[01-23]"))
-print(regular_expressions("[02-16]"))
-print(regular_expressions("[02-31]"))
-print(regular_expressions("[ 6-03]"))
-print(regular_expressions("ignored [08-11] ignored"))
-print(regular_expressions("[3] [12-04] [09-tenth]"))
-print(regular_expressions("[02-00]"))
-print(regular_expressions("[02-[08-11]04]"))
+
+def two(arg=None):
+    if not arg:
+        return 2
+    else:
+        return eval(f'2{arg}')
+
+
+def three(arg=None):
+    if not arg:
+        return 3
+    else:
+        return eval(f'3{arg}')
+
+
+def four(arg=None):
+    if not arg:
+        return 4
+    else:
+        return eval(f'4{arg}')
+
+
+def five(arg=None):
+    if not arg:
+        return 5
+    else:
+        return eval(f'5{arg}')
+
+
+def six(arg=None):
+    if not arg:
+        return 6
+    else:
+        return eval(f'6{arg}')
+
+
+def seven(arg=None):
+    if not arg:
+        return 7
+    else:
+        return eval(f'7{arg}')
+
+
+def eight(arg=None):
+    if not arg:
+        return 8
+    else:
+        return eval(f'8{arg}')
+
+
+def nine(arg=None):
+    if not arg:
+        return 9
+    else:
+        return eval(f'9{arg}')
+
+
+def plus(arg):
+    return f'+{arg}'
+
+
+def minus(arg):
+    return f'-{arg}'
+
+
+def times(arg):
+    return f'*{arg}'
+
+
+def divided_by(arg):
+    return f'//{arg}'
+
+
+print(seven(times(five())))
+print(four(plus(nine())))
+print(eight(minus(three())))
+print(six(divided_by(two())))

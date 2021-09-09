dbcf0f6b69f939ff056b24c503d102f7186d2db9
melxiory <sibagatullinnail@gmail.com>
2021-09-09 19:49:46 +0300

    Codewars, 29, как правильно, дома из знаков!!!

--- traninig.py
+++ traninig.py
@@ -1,18 +1,17 @@
 def my_crib(n):
-    home_roof = f'{"  " * n}{"_" * ((n - 1) * 2 + 3)}{"  " * n}\n'
-    home_side = ''
-    for roof_side in range(n * 2 - 1):
-        home_roof += f'{" " * (n * 2 - roof_side - 1)}/{"_" * ((n - 1) * 2 + 3) + "__" * roof_side}\\{" " * (n * 2 - roof_side - 1)}\n'
-        if n * 2 - (roof_side+1) > n:
-            home_side += f'|{"     " + " " * ((n - 1) * 6)}|\n'
-        elif n * 2 - (roof_side+1) == n:
-            home_side += f'|{" " * (n*2-1)} {"_" * (n*2-1)} {" " * (n*2-1)}|\n'
-        else:
-            home_side += f'|{" " * (n*2-1)}|{" " * (n*2-1)}|{" " * (n*2-1)}|\n'
-    else:
-        home_roof += f'/{"_____" + "_" * ((n - 1) * 6)}\\\n'
-        home_side += f'|{"_" * (n*2-1)}|{"_" * (n*2-1)}|{"_" * (n*2-1)}|'
-    return home_roof + home_side
+    wide = 4 + 3 + 6 * (n - 1)
+    door = 2 + n - 1
+    roof = 3 + 2 * (n - 1)
+    r = '{0}{1}{0}\n'.format(' ' * (wide // 2 - n), '_' * (3 + 2 * (n - 1)))
+    for i in range(1, roof):
+        r += '{0}/{1}\\{0}\n'.format(' ' * (wide // 2 - n - i), '_' * (3 + 2 * (n - 1) + 2 * (i -1)))
+    for i in range(roof - 1 - door):
+        r += '|{}|\n'.format(' ' * (wide - 2))
+    r += '|{0}{1}{0}|\n'.format(' ' * ((wide - 1) // 3), '_' * (1 + 2 * (n - 1)))
+    for i in range(1, door - 1):
+        r += '|{0}|{0}|{0}|\n'.format(' ' * ((wide - 2) // 3))
+    return r + '|{0}|{0}|{0}|'.format('_' * ((wide - 2) // 3))
+
 
 
 print(my_crib(1))

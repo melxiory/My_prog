4c06394534d48e6049b929c14476d06cdc4fa654
melxiory <sibagatullinnail@gmail.com>
2021-10-14 19:41:17 +0300

    Решение задачи 7 по книге!!!

--- traninig.py
+++ traninig.py
@@ -1,79 +1,37 @@
-class Set:
-    def __init__(self, value=[]):
-        self.data = []
-        self.concat(value)
+class Lunch:
+    def __init__(self):
+        self.customer = Customer()
+        self.employee = Employee()
 
-    def intersect(self, other):
-        res = []
-        for x in self.data:
-            if x in other:
-                res.append(x)
-        return Set(res)
+    def order(self, foodName):
+        self.customer.placeOrder(foodName, self.employee)
 
-    def union(self, other):
-        res = self.data[:]
-        for x in other:
-            res.append(x)
-        return Set(res)
-
-    def concat(self, value):
-        for x in value:
-            if not x in self.data:
-                self.data.append(x)
-
-    def __len__(self):
-        return len(self.data)
-
-    def __getitem__(self, key):
-        return self.data[key]
-
-    def __and__(self, other):
-        return self.intersect(other)
-
-    def __or__(self, other):
-        return self.union(other)
-
-    def __repr__(self):
-        return 'Set:' + repr(self.data)
-
-    def __iter__(self):
-        return iter(self.data)
+    def result(self):
+        self.customer.printFood()
 
 
-class MultiSet(Set):
-    def intersect(self, *others):
-        res = []
-        for x in self:
-            for other in others:
-                if x not in other:
-                    break
-            else:
-                res.append(x)
-        return Set(res)
+class Customer:
+    def __init__(self):
+        self.food = None
 
-    def union(*args):
-        res = []
-        for seq in args:
-            for x in seq:
-                if not x in res:
-                    res.append(x)
-        return Set(res)
+    def placeOrder(self, foodName, employee):
+        self.food = employee.takeOrder((foodName))
+
+    def printFood(self):
+        print('Food name - ', self.food.name)
 
 
-x = Set([1, 2, 3, 4])
-y = Set([11, 12, 13, 14])
-print(x | y)
-print(x.intersect(y))
-z = Set('front')
-print(z[2])
-for i in z:
-    print(i)
-print(z | 'back')
-print(z & 'back')
-X = MultiSet([1, 2, 4, 6, 8])
-Y = MultiSet([1, 3, 5, 6, 8])
-Z = MultiSet([2, 4, 6, 9, 10])
-print(X|Y, Z|Y, Z|X)
-print(X&Y, Z&Y, X&Z)
-print(X.intersect(Y,Z))
-print(X.union(Y,Z))
+class Employee:
+    def takeOrder(self, foodName):
+        return Food(foodName)
+
+
+class Food:
+    def __init__(self, name):
+        self.name = name
+
+x = Lunch()
+x.order('bigmac')
+x.result()
+x.order('pizza')
+x.result()

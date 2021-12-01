d3d6f0ecb4650478163b2b8f53e7bd0a87eac82a
melxiory <sibagatullinnail@gmail.com>
2021-12-01 17:18:45 +0300

    Codewars, 39 задача, сбаланчированные скобки!!!

--- traninig.py
+++ traninig.py
@@ -14,11 +14,17 @@ def paren(left, right=None):
             for p in paren(left, right-1): # second recursion
                 yield ")"+p
 
-def balanced_parens(n):
-    return [s for s in paren(n)]
+def balanced_parens(n, k):
+    parens = []
+    for i in paren(n):
+        parens.append(i)
+        if len(parens) > k:
+            break
+    return parens[k] if len(parens) > k > -1 else None
 
 
-print(balanced_parens(0))
-print(balanced_parens(2))
-print(balanced_parens(3))
-print(balanced_parens(4))
+print(balanced_parens(0, 0))
+print(balanced_parens(1, 0))
+print(balanced_parens(2, 0))
+print(balanced_parens(3, 5))
+print(balanced_parens(3, -1))

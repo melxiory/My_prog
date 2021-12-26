a68067eb90d6961d76146bb356763a0c2f4f7e1f
melxiory <sibagatullinnail@gmail.com>
2021-12-26 20:03:46 +0300

    Stepic, Задача: непрерывный рюкзак!!

--- traning.py
+++ traning.py
@@ -2,29 +2,43 @@ import sys
 
 
 def fetch_input():
+    global v
     segments = []
-    n = int(sys.stdin.readline())
+    n, v = tuple([int(i) for i in sys.stdin.readline().strip().split()])
     for _ in range(n):
         segments.append(tuple([int(i) for i in sys.stdin.readline().strip().split()]))
     return segments
 
 
 segments = fetch_input()
-segments.sort(key=lambda x: x[1])
+segments.sort(key=lambda x: x[0]/x[1])
+segments.reverse()
 
 
-def act_sel(list_seg):
+def knap_sack(list_seg):
     list_rez = []
     for i in list_seg:
         if not list_rez:
-            list_rez.append(i)
+            if v < i[1]:
+                list_rez.append(i[0]*(v/i[1]))
+                break
+            else:
+                list_rez.append(i[0])
+                list_rez.append(i[1])
+            if v == i[1]:
+                list_rez.append(i[0])
+                break
             continue
-        if i[0] <= list_rez[-1][1] <= i[1]:
-            continue
+        if list_rez[1] + i[1] > v:
+            list_rez[0] += i[0]*((v-list_rez[1])/i[1])
+            break
+        elif list_rez[1] + i[1] == v:
+            list_rez[0] += i[0]
+            break
         else:
-            list_rez.append(i)
-    list_rez = [i[1] for i in list_rez]
-    return len(list_rez), list_rez
+            list_rez[0] += i[0]
+            list_rez[1] += i[1]
+    return f'{list_rez[0]:.3f}'
 
 
-print(act_sel(segments))
+print(knap_sack(segments))

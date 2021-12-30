8b024edf5ec431259447df19ad47bef4c4bf0ca8
melxiory <sibagatullinnail@gmail.com>
2021-12-30 17:29:11 +0300

    Stepic, Задача: очередь с приоритетатами!!

--- traning.py
+++ traning.py
@@ -1,36 +1,13 @@
-import sys
+import heapq
+
+n = int(input())
+list_comm = [i for i in [input().split() for _ in range(n)]]
+list_heap = []
 
 
-def fetch_input():
-    segments = []
-    n = tuple([int(i) for i in sys.stdin.readline().strip().split()])[0]
-    for _ in range(n):
-        segments.append(tuple([i for i in sys.stdin.readline().strip().split(':')]))
-    segments = {j.strip(): i for i, j in segments}
-    cod_1 = sys.stdin.readline()
-    return segments, cod_1[:len(cod_1) - 1]
 
-
-segments = fetch_input()
-
-
-def huffman_decode(seg):
-    dict_dec, code = seg
-    dict_rez = []
-    summator = 0
-    while code:
-        if code[:summator] in dict_dec:
-            dict_rez.append(dict_dec[code[:summator]])
-            code = code[summator:]
-            summator = 0
-        else:
-            summator += 1
-    return ''.join(dict_rez)
-
-
-def main():
-    rez = huffman_decode(segments)
-    print(rez)
-
-
-main()
+for i in list_comm:
+    if i[0] == 'Insert':
+        heapq.heappush(list_heap, int(i[1])*(-1))
+    if i[0] == 'ExtractMax':
+        print(heapq.heappop(list_heap)*(-1))

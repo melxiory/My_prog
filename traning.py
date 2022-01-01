cc946a387f58dfa728acf3a03b38159f2b33a573
melxiory <sibagatullinnail@gmail.com>
2022-01-01 17:42:10 +0300

    Stepic, Задача: двочный поиск!!

--- traning.py
+++ traning.py
@@ -1,13 +1,19 @@
-import heapq
+import sys
 
-n = int(input())
-list_comm = [i for i in [input().split() for _ in range(n)]]
-list_heap = []
+n = list(map(int, sys.stdin.readline().strip().split()))[1:]
+k = list(map(int, sys.stdin.readline().strip().split()))[1:]
 
-
-
-for i in list_comm:
-    if i[0] == 'Insert':
-        heapq.heappush(list_heap, int(i[1])*(-1))
-    if i[0] == 'ExtractMax':
-        print(heapq.heappop(list_heap)*(-1))
+for i in k:
+    l, r = 0, len(n) - 1
+    while l <= r:
+        m = (l + r) // 2
+        if n[m] == i:
+            rez = m+1
+            break
+        if n[m] > i:
+            r = m - 1
+        else:
+            l = m + 1
+    else:
+        rez = -1
+    print(rez)

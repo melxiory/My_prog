efcd3936b676eecbaa87a9bfabe39a9845e5ff81
melxiory <sibagatullinnail@gmail.com>
2022-01-02 11:52:57 +0300

    Stepic, Задача: сортировка слиянием!!

--- traning.py
+++ traning.py
@@ -1,19 +1,41 @@
 import sys
 
-n = list(map(int, sys.stdin.readline().strip().split()))[1:]
-k = list(map(int, sys.stdin.readline().strip().split()))[1:]
+r = int(sys.stdin.readline())
+A = list(map(int, sys.stdin.readline().strip().split()[:n]))
 
-for i in k:
-    l, r = 0, len(n) - 1
-    while l <= r:
-        m = (l + r) // 2
-        if n[m] == i:
-            rez = m+1
+revers = 0
+
+
+def merge(i, j):
+    global revers
+    Q = [0]
+    while i or j:
+        if not i:
+            Q += j
             break
-        if n[m] > i:
-            r = m - 1
+        if not j:
+            Q += i
+            break
+        if i[0] > j[0]:
+            Q += [j[0]]
+            revers += len(i)
+            del j[0]
         else:
-            l = m + 1
+            Q += [i[0]]
+            del i[0]
+    del Q[0]
+    return Q
+
+
+def merge_sort(spis):
+    dlin = len(spis)
+    if dlin > 1:
+        m = dlin // 2
+        return merge(merge_sort(spis[:m + 1 if dlin > 2 and dlin % 2 else m]),
+                     merge_sort(spis[m + 1 if dlin > 2 and dlin % 2 else m:]))
     else:
-        rez = -1
-    print(rez)
+        return spis
+
+
+merge_sort(A)
+print(revers)

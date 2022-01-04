6f1ab83cb79e19f31c80159140c070b2d3882611
melxiory <sibagatullinnail@gmail.com>
2022-01-04 17:25:24 +0300

    Stepic, Задача: сортировка подсчетом!!

--- traning.py
+++ traning.py
@@ -1,66 +1,16 @@
-import sys, random, bisect
+import sys
 
-n = int(sys.stdin.readline().strip().split()[0])
-left_points, right_points = [0], [0]
-for _ in range(n):
-    i = tuple(map(int, sys.stdin.readline().strip().split()))
-    left_points += [i[0]]
-    right_points += [i[1]]
-else:
-    del left_points[0]
-    del right_points[0]
-points = list(map(int, sys.stdin.readline().strip().split()))
+n = int(sys.stdin.readline())
+list_A = [int(i) for i in sys.stdin.readline().strip().split()]
+min_A = min(list_A)
+k = (max(list_A) - min_A + 1)
+list_B = [0] * k
+for i in list_A:
+    list_B[i - min_A] += 1
+pos = 0
+for j in range(0, k):
+    for i in range(list_B[j]):
+        list_A[pos] = str(j + min_A)
+        pos += 1
 
-
-def partition3(A, l, r):
-    lt = l
-    i = l
-    gt = r
-    pivot = A[l]
-    while i <= gt:
-        if A[i] < pivot:
-            A[lt], A[i] = A[i], A[lt]
-            lt += 1
-            i += 1
-        elif A[i] > pivot:
-            A[i], A[gt] = A[gt], A[i]
-            gt -= 1
-        else:
-            i += 1
-
-    return lt, gt
-
-
-def quick_sort(A, l, r):
-    if l >= r:
-        return
-    k = random.randint(l, r)
-    A[k], A[l] = A[l], A[k]
-    lt, gt = partition3(A, l, r)
-    quick_sort(A, l, lt - 1)
-    quick_sort(A, gt + 1, r)
-
-
-def left_segm_p(k):
-    acc = 0
-    for i in left_points:
-        if k >= i:
-            acc += len(left_points[left_points.index(i):])
-            break
-    return acc
-
-
-def right_segm_p(k):
-    acc = 0
-    for i in right_points:
-        if k > i:
-            acc += len(right_points[right_points.index(i):])
-            break
-    return acc
-
-
-quick_sort(left_points, 0, len(left_points)-1)
-quick_sort(right_points, 0, len(right_points)-1)
-for i in range(len(points)):
-    points[i] = str(bisect.bisect_right(left_points, points[i]) - bisect.bisect_left(right_points, points[i]))
-print(' '.join(points))
+print(' '.join(list_A))

ce79014df942c51fcfc5e076888652419d9e4ee8
melxiory <sibagatullinnail@gmail.com>
2022-01-08 12:09:29 +0300

    Stepic, Задача: лестница, динамическое программирование!!

--- traning.py
+++ traning.py
@@ -1,12 +1,8 @@
 import sys
 
-W, n = [int(i) for i in sys.stdin.readline().strip().split()]
-list_W = [int(i) for i in sys.stdin.readline().strip().split()]
-H = [1] + [0]*W
-for i in range(n):
-    for j in range(W, list_W[i] - 1, -1):
-        if H[j - list_W[i]] == 1:
-            H[j] = 1
-while H[W] == 0:
-    W -= 1
-print(W)
+n = int(sys.stdin.readline())
+list_A = [int(i) for i in sys.stdin.readline().strip().split()]
+list_B = [0] + [list_A[0]] + [False]*(n-1)
+for i in range(1, len(list_A)):
+    list_B[i+1] = max(list_B[i-1], list_B[i]) + list_A[i]
+print(list_B[-1])

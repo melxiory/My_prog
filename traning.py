aca1195273c94053458277a3c58b75b453bdb395
melxiory <sibagatullinnail@gmail.com>
2022-01-11 16:05:47 +0300

    Stepic, Задача: стек с поддержкой максимума!!

--- traning.py
+++ traning.py
@@ -1,30 +1,21 @@
 import sys
 
-size, n = tuple(int(i) for i in sys.stdin.readline().strip().split())
-packet = [tuple(int(i) for i in sys.stdin.readline().strip().split()) for _ in range(n)]
-t_end, len_turn, sm = 0, 0, 1
-list_turn = []
-for i, (t_a, t_d) in enumerate(packet):
-    if not list_turn:
-        list_turn.append(t_a)
-        t_end, len_turn = t_a + t_d, len_turn+1
-        continue
-    if len_turn < size and t_a <= t_end:
-        list_turn.append(t_end)
-        t_end += t_d
-        len_turn += 1
-    elif t_a < t_end and len_turn == size:
-        if size < 2:
-            list_turn.append(-1)
-        elif list_turn[i-len_turn+sm] <= t_a:
-            list_turn.append(t_end)
-            t_end += t_d
+n = int(sys.stdin.readline())
+packet = [tuple(int(i) if i.isdigit() else i for i in sys.stdin.readline().strip().split()) for _ in range(n)]
+stack = []
+stack_max = []
+max_num = 0
+for comm in packet:
+    if comm[0] == 'push':
+        stack.append(comm[1])
+        if max_num < comm[1]:
+            max_num = comm[1]
+            stack_max.append(max_num)
         else:
-            list_turn.append(-1)
-            sm -= 1
-    elif t_a >= t_end:
-        list_turn.append(t_a)
-        t_end, len_turn, sm = t_a + t_d, 1, 1
-
-for i in list_turn:
-    print(i)
+            stack_max.append(max_num)
+    if comm[0] == 'max':
+        print(max_num)
+    if comm[0] == 'pop' and stack:
+        del stack[-1]
+        del stack_max[-1]
+        max_num = stack_max[-1] if stack_max else 0

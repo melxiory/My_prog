42edde820ac3f236ac22c788f6ba29249274aee7
melxiory <sibagatullinnail@gmail.com>
2022-01-11 14:01:59 +0300

    Stepic, Задача: симуляция обработки сетевых пакетов!!

--- traning.py
+++ traning.py
@@ -1,15 +1,30 @@
 import sys
 
-n = int(sys.stdin.readline())
-parent = [int(i) for i in sys.stdin.readline().strip().split()]
+size, n = tuple(int(i) for i in sys.stdin.readline().strip().split())
+packet = [tuple(int(i) for i in sys.stdin.readline().strip().split()) for _ in range(n)]
+t_end, len_turn, sm = 0, 0, 1
+list_turn = []
+for i, (t_a, t_d) in enumerate(packet):
+    if not list_turn:
+        list_turn.append(t_a)
+        t_end, len_turn = t_a + t_d, len_turn+1
+        continue
+    if len_turn < size and t_a <= t_end:
+        list_turn.append(t_end)
+        t_end += t_d
+        len_turn += 1
+    elif t_a < t_end and len_turn == size:
+        if size < 2:
+            list_turn.append(-1)
+        elif list_turn[i-len_turn+sm] <= t_a:
+            list_turn.append(t_end)
+            t_end += t_d
+        else:
+            list_turn.append(-1)
+            sm -= 1
+    elif t_a >= t_end:
+        list_turn.append(t_a)
+        t_end, len_turn, sm = t_a + t_d, 1, 1
 
-list_a = [[] for _ in range(n + 1)]
-for i in range(n):
-  list_a[parent[i]] += [i]
-r, l = list_a[-1], 0
-while r:
-  q, r = r, []
-  for b in q:
-    r += list_a[b]
-  l += 1
-print(l)
+for i in list_turn:
+    print(i)

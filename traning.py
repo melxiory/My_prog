58e9a3d1b9e32222fba0a221fccf436cc0c5da4c
melxiory <sibagatullinnail@gmail.com>
2022-01-20 16:01:04 +0300

    Stepic, Задача: проверка свойства дерева поиска, АВЛ-деревья!!

--- traning.py
+++ traning.py
@@ -1,49 +1,42 @@
 import sys
 
+sys.setrecursionlimit(10000)
 n = int(sys.stdin.readline())
 list_val1 = [[int(i) for i in sys.stdin.readline().strip().split()] for _ in range(n)]
+
+T_F = [True]
+acc = [float('-inf')]
+
+
+class Catapult(Exception):
+    pass
 
 
 def in_order(i=0):
     if list_val1[i][1] == -1:
-        print(list_val1[i][0], end=' ')
+        if acc[0] > list_val1[i][0]:
+            T_F[0] = False
+            raise Catapult
+        else:
+            acc[0] = list_val1[i][0]
         if list_val1[i][2] != -1:
             in_order(list_val1[i][2])
         return
     in_order(list_val1[i][1])
-    print(list_val1[i][0], end=' ')
+    if acc[0] > list_val1[i][0]:
+        T_F[0] = False
+        raise Catapult
+    else:
+        acc[0] = list_val1[i][0]
     if list_val1[i][2] != -1:
         in_order(list_val1[i][2])
 
 
-def pre_order(i=0):
-    if list_val1[i][1] == -1:
-        print(list_val1[i][0], end=' ')
-        if list_val1[i][2] != -1:
-            pre_order(list_val1[i][2])
-        return
-    print(list_val1[i][0], end=' ')
-    pre_order(list_val1[i][1])
-    if list_val1[i][2] != -1:
-        pre_order(list_val1[i][2])
-
-
-def post_order(i=0):
-    if list_val1[i][1] == -1:
-        if list_val1[i][2] != -1:
-            post_order(list_val1[i][2])
-            print(list_val1[i][0], end=' ')
-        else:
-            print(list_val1[i][0], end=' ')
-        return
-    post_order(list_val1[i][1])
-    if list_val1[i][2] != -1:
-        post_order(list_val1[i][2])
-    print(list_val1[i][0], end=' ')
-
-
-in_order()
-print()
-pre_order()
-print()
-post_order()
+if n == 0:
+    print('CORRECT')
+else:
+    try:
+        in_order()
+        print('CORRECT')
+    except Catapult:
+        print('INCORRECT')

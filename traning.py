b79ae052c9313f0322f7eb8af92d5d5159742ab1
melxiory <sibagatullinnail@gmail.com>
2022-03-02 21:31:14 +0300

    Stepic, Задача по Python: Оcновы и применение: имитация наследования!!

--- traning.py
+++ traning.py
@@ -1,19 +1,22 @@
-lst = [[i for i in input().split()] for _ in range(int(input()))]
-dict_ns = {'global': [['global'], []]}
-for i in lst:
-    if i[0] == 'create':
-        dict_ns[i[2]][1].append(i[1])
-        dict_ns[i[1]] = [[i[1]], []]
-        dict_ns[i[1]][0].extend(dict_ns[i[2]][0])
-    elif i[0] == 'add' and i[1] in dict_ns:
-        dict_ns[i[1]][1].append(i[2])
-    else:
-        for j in dict_ns.get(i[1])[0]:
-            if i[2] in dict_ns[j][1]:
-                print(j)
+dct_cls = {i[0]: i[1:] if len(i) > 1 else [] for i in
+           [[i for i in input().split() if i.isalpha()] for _ in range(int(input()))]}
+lst_req = [input().split() for _ in range(int(input()))]
+for j in lst_req:
+    if len(j) > 1 and j[1] in dct_cls:
+        stack, r = dct_cls[j[1]], 0
+        while r < len(stack):
+            if j[0] in stack or j[0] == j[1]:
+                print('Yes')
                 break
+            else:
+                for q in dct_cls.get(stack[r], ''):
+                    if q not in stack:
+                        stack.append(q)
+                r += 1
         else:
-            print(None)
-
-
-
+            if j[0] == j[1] and j[0] in dct_cls:
+                print('Yes')
+            else:
+                print('No')
+    else:
+        print('No')

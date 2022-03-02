d662e0f085044fe88806993a7b7b6d5e38eb17e6
melxiory <sibagatullinnail@gmail.com>
2022-03-02 16:25:28 +0300

    Stepic, Задача по Python: Омновы и применение: имитация адресного пространства!!

--- traning.py
+++ traning.py
@@ -1,44 +1,19 @@
-import sys
-
-
-def get_page(logical_addr: int):
-    pml4 = (logical_addr >> 39) & 0x1ff
-    dir_ptr = (logical_addr >> 30) & 0x1ff
-    directory = (logical_addr >> 21) & 0x1ff
-    table = (logical_addr >> 12) & 0x1ff
-    offset = logical_addr & 0xfff
-
-    return (pml4, dir_ptr, directory, table, offset)
-
-
-def get_page_phy_addr(value: int):
-    return (value & ((0xffffffffff) << 12))
-
-
-def get_phy_addr(page: tuple, mem_struct: dict, cr3: int):
-    value = cr3
-
-    for i in range(len(page) - 1):
-        index = page[i] * 8
-        value = mem_struct.get(index + value, 0)
-
-        if value & 1 == 0:
-            print("fault")
-            return
-
-        value = get_page_phy_addr(value)
-
-    print(value + page[-1])
-
-f = open('dataset_44327_15.txt', 'r')
-
-m, q, r = tuple(map(int, f.readline().strip().split()))
-m_str = dict([[int(i) for i in f.readline().strip().split()] for _ in range(m)])
-log_addr = [int(f.readline().strip()) for _ in range(q)]
-
-for i in log_addr:
-    p = get_page(i)
-    get_phy_addr(p, m_str, r)
+lst = [[i for i in input().split()] for _ in range(int(input()))]
+dict_ns = {'global': [['global'], []]}
+for i in lst:
+    if i[0] == 'create':
+        dict_ns[i[2]][1].append(i[1])
+        dict_ns[i[1]] = [[i[1]], []]
+        dict_ns[i[1]][0].extend(dict_ns[i[2]][0])
+    elif i[0] == 'add' and i[1] in dict_ns:
+        dict_ns[i[1]][1].append(i[2])
+    else:
+        for j in dict_ns.get(i[1])[0]:
+            if i[2] in dict_ns[j][1]:
+                print(j)
+                break
+        else:
+            print(None)
 
 
 

f8d7d3ec81d666d4279445c0f33889d087925291
melxiory <sibagatullinnail@gmail.com>
2022-03-05 20:45:07 +0300

    Stepic, Задача по Python: Оcновы и применение: итерация на основе класса!!

--- traning.py
+++ traning.py
@@ -1,46 +1,57 @@
-dct_cls = {i[0]: i[1:] if len(i) > 1 else [] for i in
-           [[i for i in input().split() if i.isalpha()] for _ in range(int(input()))]}
-lst_req = [input() for _ in range(int(input()))]
-stack = []
-dct_er = {i: set() for i in dct_cls}
-for i in dct_cls:
-    for q, r in dct_cls.items():
-        if i in r:
-            dct_er[i].add(q)
-            if dct_cls[i]:
-                dct_er[i].add(q)
-r = 0
-for j in lst_req:
-    if j in stack:
-        print(j)
-    elif j in dct_er:
-        stack.append(j)
-        while r < len(stack):
-            for q in dct_er.get(stack[r], ''):
-                if q not in stack:
-                    stack.append(q)
-            r += 1
+class multifilter:
+    def judge_half(pos, neg):
+        if pos >= neg:
+            return True
+        else:
+            return False
+
+    def judge_any(pos, neg):
+        if pos >= 1:
+            return True
+        else:
+            return False
+
+    def judge_all(pos, neg):
+        if neg == 0:
+            return True
+        else:
+            return False
+
+    def __init__(self, iterable, *funcs, judge=judge_any):
+        self.it = iterable
+        self.func = funcs
+        self.judge = judge
+
+    def __iter__(self):
+        for i in self.it:
+            pos, neg = 0, 0
+            for j in self.func:
+                if j(i):
+                    pos += 1
+                else:
+                    neg += 1
+            else:
+                if self.judge(pos, neg):
+                    yield i
 
 
+def mul2(x):
+    return x % 2 == 0
+
+def mul3(x):
+    return x % 3 == 0
+
+def mul5(x):
+    return x % 5 == 0
 
 
+a = [i for i in range(31)] # [0, 1, 2, ... , 30]
 
-parents = {}
-for _ in range(int(input())):
-    a = input().split()
-    parents[a[0]] = [] if len(a) == 1 else a[2:]
+print(list(multifilter(a, mul2, mul3, mul5)))
+# [0, 2, 3, 4, 5, 6, 8, 9, 10, 12, 14, 15, 16, 18, 20, 21, 22, 24, 25, 26, 27, 28, 30]
 
-def is_parent(child, parent):
-    if child == parent: return True
-    for p in parents[child]:
-        if is_parent(p, parent ): return True
-    return False
+print(list(multifilter(a, mul2, mul3, mul5, judge=multifilter.judge_half)))
+# [0, 6, 10, 12, 15, 18, 20, 24, 30]
 
-exceptions = []
-for _ in range(int(input())):
-    a = input().strip()
-    for i in exceptions:
-        if is_parent(a,i):
-            print(a)
-            break
-    exceptions.append(a)
+print(list(multifilter(a, mul2, mul3, mul5, judge=multifilter.judge_all)))
+# [0, 30]

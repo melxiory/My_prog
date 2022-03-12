df1f88c6a69ad5adfdc8726175fdd84f1cddb36b
melxiory <sibagatullinnail@gmail.com>
2022-03-12 19:59:20 +0300

    Stepic, Задача по Python: Оcновы и применение: парсинг доменов в HTML-файле!!

--- traning.py
+++ traning.py
@@ -1,57 +1,10 @@
-class multifilter:
-    def judge_half(pos, neg):
-        if pos >= neg:
-            return True
-        else:
-            return False
-
-    def judge_any(pos, neg):
-        if pos >= 1:
-            return True
-        else:
-            return False
-
-    def judge_all(pos, neg):
-        if neg == 0:
-            return True
-        else:
-            return False
-
-    def __init__(self, iterable, *funcs, judge=judge_any):
-        self.it = iterable
-        self.func = funcs
-        self.judge = judge
-
-    def __iter__(self):
-        for i in self.it:
-            pos, neg = 0, 0
-            for j in self.func:
-                if j(i):
-                    pos += 1
-                else:
-                    neg += 1
-            else:
-                if self.judge(pos, neg):
-                    yield i
-
-
-def mul2(x):
-    return x % 2 == 0
-
-def mul3(x):
-    return x % 3 == 0
-
-def mul5(x):
-    return x % 5 == 0
-
-
-a = [i for i in range(31)] # [0, 1, 2, ... , 30]
-
-print(list(multifilter(a, mul2, mul3, mul5)))
-# [0, 2, 3, 4, 5, 6, 8, 9, 10, 12, 14, 15, 16, 18, 20, 21, 22, 24, 25, 26, 27, 28, 30]
-
-print(list(multifilter(a, mul2, mul3, mul5, judge=multifilter.judge_half)))
-# [0, 6, 10, 12, 15, 18, 20, 24, 30]
-
-print(list(multifilter(a, mul2, mul3, mul5, judge=multifilter.judge_all)))
-# [0, 30]
+import requests as req, re
+a = input()
+res = req.get(a)
+lst_dom = []
+for i in re.findall(r'<\w .*href=(.http://|.https://|.ftp://|\'|\")(.+?[\.](\w\w|\w\w\w))[^\.\w\d]', res.text):
+    if i[1] not in lst_dom:
+        lst_dom.append(i[1])
+else:
+    lst_dom.sort()
+    print(*lst_dom, sep='\n')

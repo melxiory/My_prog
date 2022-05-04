31a86ae11bb07b7b8b34402fd14280531c55a578
melxiory <sibagatullinnail@gmail.com>
2022-05-04 14:07:46 +0300

    Stepic, Практикум по программированию: Задача на переливание воды!!

--- traninig.py
+++ traninig.py
@@ -1,46 +1,54 @@
-class remember:
-    def __init__(self, cls):
-        self.cls = cls
-        self.dict_state = {}
-
-    def __call__(self, *args):
-        args = args[0] if len(args) == 1 else args
-        if args not in self.dict_state:
-            if args and type(args) != int and len(args) == 2:
-                x, y = args
-                self.dict_state[args] = self.cls(x, y)
-            elif not args:
-                self.dict_state[args] = self.cls()
+a, b, n = int(input()), int(input()), int(input())
+lst_p, lst_rez, a1, b1 = [], [], 0, 0
+if a == n:
+    print('>A')
+elif b == n:
+    print('>B')
+else:
+    while a1 != n and b1 != n:
+        if a < b:
+            if b1 in lst_p:
+                print('Impossible')
+                break
+            if a1 == 0:
+                a1 += a
+                lst_rez.append('>A')
+            if b1 == b:
+                lst_p.append(b1)
+                b1, a1 = a1, 0
+                lst_rez.append('B>')
+                lst_rez.append('A>B')
             else:
-                self.dict_state[args] = self.cls(args)
-        return self.dict_state[args]
-
-    def __getitem__(self, item):
-        return self.dict_state[item]
-
-    def __iter__(self):
-        return iter(self.dict_state)
-
-
-@remember
-class A(object):
-    def __init__(self, x, y=0, z=0):
-        pass
-
-
-a = A(1)
-b = A(2, 3)
-c = A(4, 5, 6)
-d = A(1)
-
-print(A[1] is a is d)
-print(A[2, 3] is b)
-print(A[4, 5, 6] is c)
-print({x for x in A})
-"""
-dict_state = {}
-    def decor(*args):
-        if args not in dict_state:
-            dict_state[args] = args
-        return dict_state[args]
-"""
+                if b1 + a1 <= b:
+                    lst_p.append(b1)
+                    b1 += a1
+                    a1 = 0
+                else:
+                    lst_p.append(b1)
+                    a1 = a - (b - b1)
+                    b1 += b - b1
+                lst_rez.append('A>B')
+        else:
+            if a1 in lst_p:
+                print('Impossible')
+                break
+            if b1 == 0:
+                b1 += b
+                lst_rez.append('>B')
+            if a1 == a:
+                lst_p.append(a1)
+                a1, b1 = b1, 0
+                lst_rez.append('A>')
+                lst_rez.append('B>A')
+            else:
+                if b1 + a1 <= a:
+                    lst_p.append(a1)
+                    a1 += b1
+                    b1 = 0
+                else:
+                    lst_p.append(a1)
+                    b1 = b - (a - a1)
+                    a1 += a - a1
+                lst_rez.append('B>A')
+    else:
+        print(*lst_rez, sep='\n')

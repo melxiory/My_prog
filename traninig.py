22fe49ae760c055f2aee28ad7130b9e7fc97234d
melxiory <sibagatullinnail@gmail.com>
2022-05-22 10:31:00 +0300

    Stepic, Практикум по программироапнию: Сапер!!

--- traninig.py
+++ traninig.py
@@ -1,10 +1,9 @@
 n, m = map(int, input().split())
-a = [[0] * m for i in range(n)]
-s = 0
-for k in range(n + m + 1):
-    for i in range(n):
-        for j in range(m):
-            if i + j + 1 == k:
-                a[i][j] = f'{s:>3}'
-                s += 1
-[print(*_, sep='') for _ in a]
+x = [tuple(map(int, input().split())) for _ in range(int(input()))]
+f = [[(0, '*')[(r + 1, c + 1) in x] for c in range(m)] for r in range(n)]
+for r in range(n):
+    for c in range(m):
+        if f[r][c] != '*':
+            f[r][c] = sum(f[i][j] == '*' for i in range(max(0, r - 1), min(r + 2, n)) 
+                                         for j in range(max(0, c - 1), min(c + 2, m)))            
+[print(*_) for _ in f]

8d62dcfb1ebf47537dea2f7518415ee0e60707a8
melxiory <sibagatullinnail@gmail.com>
2022-06-02 10:37:38 +0300

    Stepic, Практикум по программироапнию: Теорема Лагранжа!!

--- traninig.py
+++ traninig.py
@@ -1,18 +1,6 @@
 n = int(input())
-t = [[9] * n for i in range(n)]
-i, j, k, l = 0, 0, 1, 0
-for _ in range(n * n):
-    t[i][j] = k
-    if i <= j + 1 and i + j < n - 1:
-        j += 1
-    elif i < j and i + j >= n - 1:
-        i += 1
-    elif i >= j and i + j > n - 1:
-        j -= 1
-    elif i > j + 1 and i + j <= n - 1:
-        i -= 1
-    if i-1 >= 0 and j == l and k == t[i-1][j]:
-        k = 0 if k else 1
-        l += 1
-for q in t:
-    print(*q)
+ld = {i ** 2 + u ** 2: [i, u] for i in range(int(n ** 0.5) + 1) for u in range(i, int(n ** 0.5) + 1)}
+for i in ld:
+    if n - i in ld:
+        print(*list(filter(lambda x: x, ld[i] + ld[n - i]))[::-1], sep=' ')
+        break

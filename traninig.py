c709e057559b364ec30f19f4cedfb466360b9813
melxiory <sibagatullinnail@gmail.com>
2022-06-10 21:28:55 +0300

    Stepic, Практикум по программироапнию: Двоичный дополнительный код!!

--- traninig.py
+++ traninig.py
@@ -1,33 +1,5 @@
-import math
+def bindigits(n, bits):
+    s = bin(n & int("1"*bits, 2))[2:]
+    return ("{0:0>%s}" % (bits)).format(s)
 
-a, b, c = map(int, input().split())
-
-
-def qwer(a, b):
-    x = 1
-    x1 = 0
-    y = 0
-    y1 = 1
-    while b != 0:
-        q = a // b
-        r = a % b
-        x2 = x - q * x1
-        y2 = y - q * y1
-        a, b = b, r
-        x, x1 = x1, x2
-        y, y1 = y1, y2
-    return str(a), str(x), str(y)
-
-
-x, y = 0, 0
-gcds = 0
-if c % math.gcd(a, b) != 0:
-    print('Impossible')
-else:
-    gcds, x, y = map(int, qwer(a, b))
-    x *= c // gcds
-    y *= c // gcds
-    q = x // (b // gcds)
-    x %= b // gcds
-    y += a // gcds * q
-    print(math.gcd(a, b), x, y)
+print(bindigits(int(input()), int(input())))

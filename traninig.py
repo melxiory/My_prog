67a5a9afe1de823a5f28b958676141b17958d222
melxiory <sibagatullinnail@gmail.com>
2022-07-20 16:25:13 +0300

    Stepic, Практикум по математике и python: Цилиндр Джеферсона!!!

--- traninig.py
+++ traninig.py
@@ -1,23 +1,41 @@
+import random as rnd
+
+rnd.seed(42)
+clear_alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+n = 6
+text = 'Some encripted text'
+
+
 def caesar(text, key, alphabet):
     return alphabet[(alphabet.index(text) + key) % len(alphabet)]
 
 
-def jarriquez_encryption(text, key, alphabet='ABCDEFGHIJKLMNOPQRSTUVWXYZ', reverse=False):
-    st, key = '', str(key)
-    for n, i in enumerate(text):
-        if not reverse:
-            st += caesar(i, int(key[n % len(key)]), alphabet)
-        else:
-            st += caesar(i, -int(key[n % len(key)]), alphabet)
+def jarriquez_encryption(text, key, alphabet, reverse=False):
+    if not reverse:
+        return caesar(text, int(key), alphabet)
+    else:
+        return caesar(text, -int(key), alphabet)
+
+
+
+def disc_generator(alphabet):
+    lst = list(alphabet)
+    random.shuffle(lst)
+    return ''.join(lst)
+
+
+discs = [disc_generator(clear_alphabet) for _ in range(n)]
+
+
+def jefferson_encryption(text, discs, step, reverse=False):
+    t = [i.upper() for i in text if i.isalnum()]
+    n, st = 0, ''
+    for j in t:
+        if n == len(discs):
+            n = 0
+        st += jarriquez_encryption(j, step, discs[n], reverse)
+        n += 1
     return st
 
 
-for i in range(1000, 999999):
-    rez = jarriquez_encryption(
-        'ТЛБЛДУЭППТКЛФЧУВНУПБКЗИХТЛТТЫХНЛОИНУВЖММИНПФНПШОКЧЛЕРНТФНАХЖИДМЯКЛТУБЖИУЕЖЕАХЛГЩЕЕЪУВНГАХИЯШПЙАОЦЦПВТЛБФТТИИНДИДНЧЮ'
-        'ОНЯОФВТЕАТФУШБЛРЮЮЧЖДРУУШГЕХУРПЧЕУВАЭУОЙБДБНОЛСКЦБСАОЦЦПВИШЮТППЦЧНЖОИНШВРЗЕЗКЗСБЮНЙРКПСЪЖФФШНЦЗРСЭШЦПЖСЙНГЭФФВЫМЖИЛР'
-        'ОЩСЗЮЙФШФДЖОИЗТРМООЙБНФГОЩЧФЖООКОФВЙСЭФЖУЬХИСЦЖГИЪЖДШПРМЖПУПГЦНВКБНРЕКИБШМЦХЙИАМФЛУЬЙИСЗРТЕС',
-        i, alphabet='АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ',
-        reverse=True)
-    if 'АЛМАЗ' in rez and 'ДАКОСТА' in rez:
-        print(i)
+print(jefferson_encryption(text, discs, 13))
